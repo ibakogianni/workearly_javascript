9553d4ee7f7608a6dadef20b7518e169f574813b
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-11-27 20:11:23 +0200

    Assignment with alternative solutions

---
+++ dom-assignment-start/dom-assignment-start/assignment.js
@@ -0,0 +1,15 @@
+// const task1 = document.querySelector('#task-1');
+const task1 = document.getElementById('task-1');
+task1.style.color = 'white';
+task1.style.backgroundColor = "black";
+
+//const task2 = document.querySelector('title');
+const task2 = document.head;
+const task2a = task2.querySelector('title');
+task2a.textContent ='Assignment - Solved!';
+// const docTitle = document.head.querySelector('title')
+// docTitle.textContent ='Assignment - Solved!';
+
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
