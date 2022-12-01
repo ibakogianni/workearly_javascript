e2b43e0a510debf9d6f6b8fe2e2b826ab97647ec
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-01 21:59:34 +0200

    End of video 159
    Video 159_DOM Traversal vs Query Methods

--- dom-01-starting-code/dom-01-starting-code/app.js
+++ dom-01-starting-code/dom-01-starting-code/app.js
@@ -1,15 +1,3 @@
-const h1 = document.getElementById('main-title');
-h1.textContent = 'Some New title!';
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
-
-const li = document.querySelector('li:last-of-type');
-li.textContent = li.textContent + '(Changed!)';
-
-const body = document.body;
-
-//const listItemElements = document.querySelectorAll('li');
-const listItemElements = document.getElementsByTagName('li');
-for(const listItemEl of listItemElements){
-    console.dir(listItemEl);
-}
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstListItem = ul.firstElementChild;
+console.log(firstListItem);
