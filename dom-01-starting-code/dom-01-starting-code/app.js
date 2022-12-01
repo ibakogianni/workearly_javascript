24442ac6b4b45473ab9e95a15afe454354e4e643
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-01 22:39:48 +0200

    End of video 160
    Video 160 Styling DOM elements

--- dom-01-starting-code/dom-01-starting-code/app.js
+++ dom-01-starting-code/dom-01-starting-code/app.js
@@ -1,3 +1,18 @@
 const ul = document.body.firstElementChild.nextElementSibling;
 const firstListItem = ul.firstElementChild;
+const section = document.querySelector('section');
 console.log(firstListItem);
+
+//section.style.backgroundColor="blue";
+//section.className = '';
+const button = document.querySelector('button');
+button.addEventListener('click', ()=>{
+    // if(section.className ==='red-bg visible'){
+    //     section.className = 'red-bg invisible';
+    // }else{
+    //     section.className = 'red-bg visible';
+    // }
+   section.classList.toggle('visible');
+   section.classList.toggle('invisible');
+
+})
