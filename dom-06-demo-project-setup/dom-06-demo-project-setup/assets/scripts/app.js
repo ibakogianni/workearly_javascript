e5b11513f430350852e4e1e94268fae7039c55cf
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-08 22:27:52 +0200

    End of Video 174
    Video 174 Fetching and Validating User Input

--- dom-06-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.querySelector('#add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.querySelector('#backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
     toggleBackdrop();
@@ -16,6 +18,17 @@ const backdropClickHandler = ()=>{
 const cancelAddMovie = ()=>{
     toggleMovieModal();
 }
+const AddMovieHandler = ()=>{
+    const titleValue = userInputs[0].value;
+    const imgUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if(titleValue.trim()==='' || imgUrlValue.trim==='' || ratingValue.trim()=== '' || +ratingValue < 1 ||  +ratingValue > 5){
+        alert('Please enter Valid values (rating between 1 and 5');
+            return;
+    }
+};
 startAddMovieButton.addEventListener('click', toggleMovieModal );
 backdrop.addEventListener('click', backdropClickHandler);
 cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+confirmAddMovieButton.addEventListener('click', AddMovieHandler);
