c8eb46fa4a54235b25bac3a94386dedc60526b5d
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-08 22:04:57 +0200

    End of Video 173
    Video 173 Controlling the backdrop

--- dom-06-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,8 +1,21 @@
 const addMovieModal = document.querySelector('#add-modal');
 const startAddMovieButton = document.querySelector('header button');
-
+const backdrop = document.querySelector('#backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
+const toggleBackdrop = ()=>{
+    backdrop.classList.toggle('visible');
+}
+const backdropClickHandler = ()=>{
+    toggleMovieModal();
+}
+const cancelAddMovie = ()=>{
+    toggleMovieModal();
+}
 startAddMovieButton.addEventListener('click', toggleMovieModal );
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
