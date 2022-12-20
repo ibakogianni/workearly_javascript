859861c091f7d5ea3ada8a0a5c88104c9cd6cbd4
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-21 00:10:42 +0200

    End of Video 194
    Video 194_Alternatives to for Loops: The forEach() method

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -60,3 +60,14 @@ const maxIndex = personData.findIndex((person, index, persons)=>{
     return person.name === 'max';
 });
 console.log(maxIndex);
+const prices = [10.99, 5.99,3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+// for (const price of prices){
+//     taxAdjustedPrices.push(price * (1 + tax) );
+// }
+prices.forEach((price, index, prices)=>{
+    const priceObj = {index: index, taxAdjPrice: price * (1+  tax) }
+    taxAdjustedPrices.push(priceObj );
+});
+console.log(taxAdjustedPrices);
