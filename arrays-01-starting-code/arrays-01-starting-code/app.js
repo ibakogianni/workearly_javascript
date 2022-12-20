1b38ccde9898ec1a5dbc63ff91cac68cf91406f1
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-21 01:06:20 +0200

    End of video 199
    Video 199_The Importand reduce() method

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -88,3 +88,14 @@ const sortedPrices = prices.sort((a, b)=>{
 console.log(sortedPrices.reverse());
 const filteredArray = prices.filter(price=>price > 6);
 console.log(filteredArray);
+
+// let sum = 0;
+// prices.forEach((price)=>{
+//     sum +=price;
+// });
+// console.log(sum);
+
+const sum = prices.reduce((prevValue, curValue, curIndex, prices)=>{
+    return prevValue + curValue;
+},0 );
+console.log(sum);
