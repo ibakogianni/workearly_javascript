4e96fd2b4e362228e16cffe8c05b883b683172ea
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-20 23:15:33 +0200

    End of Video 190
    Video_190_Adding Arrays to Arrays with concat()

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -39,6 +39,7 @@ const removedElements = hobbies.splice(0,1);
 console.log(hobbies);
 console.log(removedElements);
 const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
-const storedResults = testResults.slice(0,2);
+// const storedResults = testResults.slice(0,2);
+const storedResults = testResults.concat([3.99,2]);
 testResults.push(5.91);
 console.log(storedResults, testResults);
