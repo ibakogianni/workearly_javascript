0b49b2ac3085c56ff8874e126664899bb74b7d20
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-21 00:35:36 +0200

    End of Video 196
    Video 196_sort()ing and reverse()ing

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -76,3 +76,13 @@ const taxAdjustedPrices= prices.map((price, index, prices)=>{
     return priceObj;
 });
 console.log(prices, taxAdjustedPrices);
+const sortedPrices = prices.sort((a, b)=>{
+    if(a > b){
+        return 1;
+    }else if (a===b){
+        return 0;
+    }else{
+        return -1;
+    }
+});
+console.log(sortedPrices.reverse());
