0777d3823dad0fc83853955ae3e1b9e40d484278
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-21 00:26:37 +0200

    End of Video 195
    Video 195_Transforming Data with map()

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -62,12 +62,17 @@ const maxIndex = personData.findIndex((person, index, persons)=>{
 console.log(maxIndex);
 const prices = [10.99, 5.99,3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
+//const taxAdjustedPrices = [];
 // for (const price of prices){
 //     taxAdjustedPrices.push(price * (1 + tax) );
 // }
-prices.forEach((price, index, prices)=>{
+// prices.forEach((price, index, prices)=>{
+//     const priceObj = {index: index, taxAdjPrice: price * (1+  tax) }
+//     taxAdjustedPrices.push(priceObj );
+// });
+//console.log(taxAdjustedPrices);
+const taxAdjustedPrices= prices.map((price, index, prices)=>{
     const priceObj = {index: index, taxAdjPrice: price * (1+  tax) }
-    taxAdjustedPrices.push(priceObj );
+    return priceObj;
 });
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
