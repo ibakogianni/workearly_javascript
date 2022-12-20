80eeda9926aaf18ed3b78dbe101e99a1b03ad3d8
ibakogianni <81587481+ibakogianni@users.noreply.github.com>
2022-12-20 21:57:12 +0200

    End of Video 185
    Video 185_Creating Arrays

---
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+const moreNumbers = new Array(5); //[]
+console.log(moreNumbers);
+
+const yetMoreNumbers = Array.of(1,2);
+console.log(yetMoreNumbers );
+
+const listItems = document.querySelectorAll('li');
+const moreNumbers2 = Array.from(listItems);
+console.log(moreNumbers2);
