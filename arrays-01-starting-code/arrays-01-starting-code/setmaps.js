da1ba850320849a183f436d67fda11ede4a6e08b
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-03 22:47:18 +0200

    End of Video 206
    Video 206-Working with Maps

--- arrays-01-starting-code/arrays-01-starting-code/setmaps.js
+++ arrays-01-starting-code/arrays-01-starting-code/setmaps.js
@@ -1,13 +1,35 @@
-const ids = new Set(['Hi','from','set!']);
-ids.add(2);
+// const ids = new Set(['Hi','from','set!']);
+// ids.add(2);
 
-if(ids.has('Hi')){
-    ids.delete('Hi');
+// if(ids.has('Hi')){
+//     ids.delete('Hi');
+// }
+
+// console.log(ids);
+// ids.entries()
+
+// for(const entry of ids.entries()){
+//     console.log(entry);
+// }
+
+const person1 = {name: 'Max'};
+const person2 = {name: 'Manuel'};
+
+const personData = new Map([[person1, [{date: 'yesterday', price: '10'}]]]);
+console.log(personData);
+personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+console.log(personData.get(person1));
+
+for(const [key, value] of personData.entries()){
+    console.log(key, value);
 }
 
-console.log(ids);
-ids.entries()
-
-for(const entry of ids.entries()){
-    console.log(entry);
+for(const key of personData.keys()){
+    console.log(key);
 }
+
+for(const value of personData.values()){
+    console.log(value);
+}
+
+console.log(personData.size);
