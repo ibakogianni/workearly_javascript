7edd213f0becb21b54b352661953337ee177affc
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-10 20:48:18 +0200

    End of video 218
    Video 218- Property types and property Order

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -7,10 +7,13 @@ age: 30,
 hobbies:['sports', 'cooking'],
 greet: function(){
     alert('Hi there');
-}
+},
+1.5 : 'hello'
 };
 //person.age=31;
 delete person.age;
 person.isAdmin =true;
 console.log(person["first-name"]);
+console.log(person[1.5]);
+console.log(person);
 //person.greet();
