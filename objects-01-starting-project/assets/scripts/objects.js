9a26fa2ae08c32501f60387ddd6717a420dcf69f
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-10 21:53:08 +0200

    End of Video 220
    Video 220- Demo App and Property Syntax

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,26 +1,49 @@
-const movieList = document.getElementById('movie-list');
-movieList.style['background-color']= 'red';
-movieList.style.display='block';
+// const movieList = document.getElementById('movie-list');
+// movieList.style['background-color']= 'red';
+// movieList.style.display='block';
 
-const  userChosenKeyName = 'level';
+// const  userChosenKeyName = 'level';
 
-let person = {
-'first-name': 'Max',
-age: 30,
-hobbies:['sports', 'cooking'],
-[userChosenKeyName]: '...',
-greet: function(){
-    alert('Hi there');
-},
-1.5 : 'hello'
-};
+// let person = {
+// 'first-name': 'Max',
+// age: 30,
+// hobbies:['sports', 'cooking'],
+// [userChosenKeyName]: '...',
+// greet: function(){
+//     alert('Hi there');
+// },
+// 1.5 : 'hello'
+// };
 //person.age=31;
-delete person.age;
-person.isAdmin =true;
+// delete person.age;
+// person.isAdmin =true;
 
-console.log(person["first-name"]);
-console.log(person[1.5]);
-console.log(person);
-const keyName = 'first-name';
-console.log(person[keyName]);
+// console.log(person["first-name"]);
+// console.log(person[1.5]);
+// console.log(person);
+// const keyName = 'first-name';
+// console.log(person[keyName]);
 //person.greet();
+
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+const movies = [];
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+    if(title.trim()=== '' || extraName.trim()=== '' || extraValue.trim()=== ''){
+        return;
+    }
+    const newMovie = {
+        info: {
+            title:title,
+           [ extraName]: extraName
+        },
+        id: Math.random()
+    };
+    movies.push(newMovie);
+    console.log(newMovie);
+};
+addMovieBtn.addEventListener('click', addMovieHandler);
