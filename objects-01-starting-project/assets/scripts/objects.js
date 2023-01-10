b6c37c0aa467eecf9d6abb3b08558d3da9e24306
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-10 20:56:17 +0200

    end of video 219
    Video 219 - Dynamic property Access and setting Properties Dynamically

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,10 +1,14 @@
 const movieList = document.getElementById('movie-list');
 movieList.style['background-color']= 'red';
 movieList.style.display='block';
+
+const  userChosenKeyName = 'level';
+
 let person = {
 'first-name': 'Max',
 age: 30,
 hobbies:['sports', 'cooking'],
+[userChosenKeyName]: '...',
 greet: function(){
     alert('Hi there');
 },
@@ -13,6 +17,7 @@ greet: function(){
 //person.age=31;
 delete person.age;
 person.isAdmin =true;
+
 console.log(person["first-name"]);
 console.log(person[1.5]);
 console.log(person);
