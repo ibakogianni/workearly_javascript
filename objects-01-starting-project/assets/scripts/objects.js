63f8ba80e541b2a64681040769e9eef50853268c
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-10 22:32:23 +0200

    End of Video 221
    Video 221-Rendering Elements basic on Objects

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -29,6 +29,26 @@ const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 const movies = [];
 
+const renderMovies = () => {
+        const movieList = document.getElementById('movie-list');
+        
+        if(movies.length === 0){
+            movieList.classList.remove('visible'); 
+            return;
+        }else {
+            movieList.classList.add('visible');
+        }
+        movieList.innerHTML='';
+        movies.forEach((movie)=>{
+            const movieEl = document.createElement('li');
+            movieEl.textContent = movie.info.title;
+            movieList.append(movieEl);
+
+        });
+    }
+
+
+
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
@@ -44,6 +64,6 @@ const addMovieHandler = () => {
         id: Math.random()
     };
     movies.push(newMovie);
-    console.log(newMovie);
+    renderMovies();
 };
 addMovieBtn.addEventListener('click', addMovieHandler);
