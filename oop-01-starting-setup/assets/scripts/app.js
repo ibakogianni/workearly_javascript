42459388eac82125a5ffca3cb1a8ee26811fe2e8
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-16 22:13:43 +0200

    End of Video 241
    Video 241 - Getting Started with OOP code

---
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,52 @@
+// const products =[
+//     {
+//         title: 'A pillow', 
+//         imageUrl: 'https://www.premierinnathome.com/wp-content/uploads/2021/02/WH45718_PI_Bed_03_2112305_RET-scaled.jpg', 
+//         price: 19.99, 
+//         description : 'A sort Pillow!'},
+//         {
+//             title: 'A carpet', 
+//             imageUrl: 'https://m.media-amazon.com/images/I/61i5XSPkngL._SL1080_.jpg', 
+//             price: 89.99, 
+//             description : 'A carpet which you might like- or not!'}
+// ];
+
+const productList = {
+    products:[
+        {
+            title: 'A pillow', 
+            imageUrl: 'https://www.premierinnathome.com/wp-content/uploads/2021/02/WH45718_PI_Bed_03_2112305_RET-scaled.jpg', 
+            price: 19.99, 
+            description : 'A sort Pillow!'
+        },
+            {
+                title: 'A carpet', 
+                imageUrl: 'https://m.media-amazon.com/images/I/61i5XSPkngL._SL1080_.jpg', 
+                price: 89.99, 
+                description : 'A carpet which you might like- or not!'}
+    ],
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className ='product-list';
+        for (const prod of this.products){
+            const prodEl =document.createElement('li');
+            prodEl.className ='product-item';
+            prodEl.innerHTML=`
+                <div>
+                    <image src="${prod.imageUrl}" alt="${prod.title}">
+                    <div class="product-item__content">
+                        <h2>${prod.title}</h2>
+                        <h3>\$${prod.price}</h3>
+                        <p>${prod.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                
+                </div>
+            `;
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
+    }
+};
+productList.render();
