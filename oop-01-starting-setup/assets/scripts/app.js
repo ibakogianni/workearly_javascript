f2cccd910164b36e01d101f8c4c69d5fffac5300
Irida Bakogianni <ibakogianni@gmail.com>
2023-01-24 21:19:33 +0200

    End of Video 245
    Video 245- Using and connecting multiple classes

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -24,34 +24,51 @@ class Product {
         this.price = price;
     }
 }
-//console.log(new Product());
-const productList = {
-    products:[
-        new Product('A pillow','https://www.premierinnathome.com/wp-content/uploads/2021/02/WH45718_PI_Bed_03_2112305_RET-scaled.jpg','A sort Pillow!',19.99),
-        new Product ('A carpet','https://m.media-amazon.com/images/I/61i5XSPkngL._SL1080_.jpg','A carpet which you might like- or not!',89.99),
-    ],
-    render() {
+
+class ProductItem {
+    constructor(product){
+        this.product = product;
+    }
+    render(){
+        const prodEl =document.createElement('li');
+        prodEl.className ='product-item';
+        prodEl.innerHTML=`
+            <div>
+                <image src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+            
+            </div>
+        `;
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
+        
+            new Product('A pillow','https://www.premierinnathome.com/wp-content/uploads/2021/02/WH45718_PI_Bed_03_2112305_RET-scaled.jpg','A sort Pillow!',19.99),
+            new Product ('A carpet','https://m.media-amazon.com/images/I/61i5XSPkngL._SL1080_.jpg','A carpet which you might like- or not!',89.99),
+        
+    ];
+
+    constructor(){}
+
+    render(){
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className ='product-list';
         for (const prod of this.products){
-            const prodEl =document.createElement('li');
-            prodEl.className ='product-item';
-            prodEl.innerHTML=`
-                <div>
-                    <image src="${prod.imageUrl}" alt="${prod.title}">
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                
-                </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
-};
+}
+const productList = new ProductList();
 productList.render();
